1e86c8d2d996efa0100784f7901335f9aff2db09
karmasakshi <karmasakshi@outlook.com>
2015-02-28 16:19:00 +0530

    Added invoices.

--- app/scripts/controllers/invoice.js
+++ app/scripts/controllers/invoice.js
@@ -8,13 +8,22 @@
  * Controller of the hcrcApp
  */
 angular.module('hcrcApp')
-        .controller('InvoiceCtrl', ['$scope', 'invoiceFactory', function ($scope, invoiceFactory) {
+        .controller('InvoiceCtrl', ['$scope', 'adminFactory', 'invoiceFactory', 'traderFactory', function ($scope, adminFactory, invoiceFactory, traderFactory) {
                 // Models
                 $scope.invoices = [];
 
                 // Get all invoices
                 invoiceFactory.query(function (data) {
-                    $scope.invoices = data;
+                    angular.forEach(data, function (invoice) {
+                        traderFactory.get({id: invoice.trader}, function (data) {
+                            invoice.trader = data.name;
+                        });
+                        invoice.date = moment(invoice.date).fromNow();
+                        adminFactory.get({id: invoice.addedby}, function (data) {
+                            invoice.addedby = data.username;
+                        });
+                        $scope.invoices.push(invoice);
+                    });
                 }, function (err) {
                     console.log(err);
                 });
